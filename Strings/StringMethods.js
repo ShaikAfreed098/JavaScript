7828e6a5074ba6b6fb7476d7e06f9513442509a8
ShaikAfreed098 <shaikafreed098@gmail.com>
2026-06-26 12:47:33 +0530

    String Methods

---
+++ Strings/StringMethods.js
@@ -0,0 +1,72 @@
+//slice method extracts a section of a string and returns it as a new string, without modifying the original string.
+let s1 = "LoveYourSelf";
+let slicedStr = s1.slice(0, 5);
+console.log(slicedStr); // Output: LoveY
+
+//substring() method returns the part of the string between the start and end indexes, or to the end of the string.
+let s2 = "LoveYourSelf";
+let subStr1 = s2.substring(0, 5);
+console.log(subStr1); // Output: LoveY
+
+//substr() method returns the part of the string starting at the specified index and extending for a given number of characters afterwards.
+let s3 = "LoveYourSelf";
+let subStr2 = s3.substr(0, 9);
+console.log(subStr2); // Output: LoveYourSelf
+
+//replace() replaces a part of the given string with another string or a regular expression. The original string will remain unchanged.
+let str = "Mind, Power, Soul";
+
+// Use the replace() method to replace the substring
+let part = str.replace("Power", "Space");
+
+// Output the resulting string after replacement
+console.log(part); // Output: Mind, Space, Soul
+
+/*More JS String Methods
+Below is the JavaScript string functions list:
+
+Instance Methods
+
+Description
+
+at()	            Find the character at the specified index.
+anchor()	        Creates an anchor element that is used as a hypertext target.
+charAt()	        Returns that character at the given index of the string.
+charCodeAt()	    Returns a Unicode character set code unit of the character present at the index in the string.
+codePointAt()	    Return a non-negative integer value i.e, the code point value of the specified element.
+concat()	        Join two or more strings together in JavaScript.
+endsWith()	        Whether the given string ends with the characters of the specified string or not.
+includes()	        Returns true if the string contains the characters, otherwise, it returns false.
+indexOf()	        Finds the index of the first occurrence of the argument string in the given string.
+lastIndexOf()	    Finds the index of the last occurrence of the argument string in the given string.
+localeCompare()	    Compare any two elements and returns a positive number
+match()	            Search a string for a match against any regular expression.
+matchAll()	        Return all the iterators matching the reference string against a regular expression.
+normalize()	        Return a Unicode normalization form of a given input string.
+padEnd()	        Pad a string with another string until it reaches the given length from rightend.
+padStart()	        Pad a string with another string until it reaches the given length from leftend.
+repeat()	        Build a new string containing a specified number of copies of the string.
+replace()	        Replace a part of the given string with some another string or a regular expression
+replaceAll()	        Returns a new string after replacing all the matches of a string with a specified string/regex.
+search()	        Search for a match in between regular expressions and a given string object.
+slice()	            Return a part or slice of the given input string.
+split()	            Separate given string into substrings using a specified separator provided in the argument.
+startsWith()	    Check whether the given string starts with the characters of the specified string or not.
+substr()	        Returns the specified number of characters from the specified index from the given string.
+substring()	        Return the part of the given string from the start index to the end index.
+toLowerCase()	    Converts the entire string to lowercase.
+toLocaleLowerCase()	Returns the calling string value converted to a lowercase letter.
+toLocaleUpperCase()	Returns the calling string value converted to a uppercase letter.
+toUpperCase()	    Converts the entire string to uppercase.
+toString()	        Return the given string itself.
+trim()	            Remove the white spaces from both ends of the given string.
+trimEnd()	        Remove white space from the end of a string.
+trimStart()	        Remove white space from the start of a string.
+valueOf()	        Return the value of the given string.
+string[Symbol.iterator]()	    This method is used to make String iterable. [@@iterator]() returns an iterator object which iterates over all code points of the String.
+fromCharCode(n1, n2, ..., nX)	This method is used to create a string from the given sequence of UTF-16 code units. This method returns a string, not a string object.
+fromCodePoint(a1, a2, a3, ....)	This method in JavaScript that is used to return a string or an element for the given sequence of code point values (ASCII value).
+isWellFormed()	                This method is used to check if the string contains a lone surrogate or not
+String.raw(str, ...sub)	        This is a static method that is used to get the raw string form of template literals. These strings do not process escape characters.
+toWellFormed()	                This method is used to return where all lone surrogates of this string are replaced with the Unicode replacement character.
+*/

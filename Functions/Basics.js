8f9d0b238974ccbf306d41949b231a5bda3abeb6
ShaikAfreed098 <shaikafreed098@gmail.com>
2026-07-17 01:50:29 +0530

    Add examples of named functions, anonymous functions, and function expressions

--- Functions/Basics.js
+++ Functions/Basics.js
@@ -11,3 +11,30 @@ greet("Alice");  // "Alice" is the argument
 
 //Parameter: name (placeholder inside the function).
 //Argument: "Alice" (real value given at call time).
+
+/*Types of Functions
+Here are all the main types of functions in JavaScript:
+NAMED FUNCTIONS:A function that has its own name when declared. It’s easy to reuse and debug because the name shows up in error messages or stack traces.
+*/
+function greet() {
+  return "Function is working";
+}
+console.log(greet());
+
+/*
+Anonymous Function
+An anonymous function is a function defined without an explicit name. It is commonly used as a callback or assigned to a variable.
+*/
+const come = function() {
+  return "Anonymous Function is working";
+};
+console.log(come());
+
+/*
+Function Expression
+A function expression is a function created as part of an expression and assigned to a variable or passed to another function. It can be named or anonymous.
+*/
+const add = function(a, b) {
+  return a + b;
+};
+console.log(add(2, 3));

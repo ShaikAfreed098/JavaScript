664d3cef24306aec0e6cef294a839f851a127a6c
ShaikAfreed098 <shaikafreed098@gmail.com>
2026-07-17 01:36:48 +0530

    Functions basics

---
+++ Functions/Basics.js
@@ -0,0 +1,13 @@
+/*In functions, parameters are placeholders defined in the function, while arguments are the actual values you pass when calling the function.
+
+Example:
+*/
+
+function greet(name) {   // 'name' is a parameter
+  console.log("Hello " + name);
+}
+​
+greet("Alice");  // "Alice" is the argument
+
+//Parameter: name (placeholder inside the function).
+//Argument: "Alice" (real value given at call time).
